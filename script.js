a5a197e9d4753f6f10280b844606ea34e39372c8
Ayoub EL AKKAL <akkalayoub3@gmail.com>
2021-12-16 23:00:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const form = document.querySelector("form"),
+const form = document.querySelector("#my-form"),
 statusTxt = form.querySelector(".button-area span");
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.color = "black";
   statusTxt.style.display = "block";
   statusTxt.innerText = "Sending your message...";
   form.classList.add("disabled");
@@ -13,7 +13,7 @@ form.addEventListener('submit', (e)=>{
   xhr.onload = ()=>{
     if(xhr.readyState == 4 && xhr.status == 200){
       let response = xhr.response;
-      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1){
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
         statusTxt.style.color = "red";
       }else{
         form.reset();
